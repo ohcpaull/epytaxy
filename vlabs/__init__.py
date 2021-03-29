0af1c98437ac1eb303aef066dd5b84916c7dab10
hlam159 <74382856+hlam159@users.noreply.github.com>
2021-03-29 14:13:58 +1100

    ADD: edited init

--- vlabs/__init__.py
+++ vlabs/__init__.py
@@ -1,6 +1,21 @@
 from .spm import scanningprobe
 from .xray import xrr, xrd
 
+from vlabs.spm.utils import(
+    gaussian,
+    skewed_gauss,
+    rayleigh,
+    exp_dist,
+    lorentz,
+    line,
+    parabola,
+    second_poly,
+    cubic,
+    exp,
+    log,
+    sine,
+    cosine,
+)
 
 try:
     from refnx.version import version as __version__
